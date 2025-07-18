ab593b3990e8ee1facf080131112e9d8cbf499b4
Dooky <chamallow14@hotmail.fr>
2025-07-17 23:24:00 -0300

    spaces in time 25 : 00 in HTML, test in CSS, short break function in JS

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let isRunning = false;
 
 /*******************************TIMER DISPLAY*******************************/
 function displayTimer (){
-// (seconds < 10 ? '0' : '') + seconds => if the second goes down 10 like 7 this is gone add 0 infront resulting 07   
-document.querySelector("#time").textContent = `${minutes} : ${(seconds < 10 ? '0' : '') + seconds}`;
+    // (seconds < 10 ? '0' : '') + seconds => if the second goes down 10 like 7 this is gone add 0 infront resulting 07
+    document.querySelector("#time").textContent = `${(minutes < 10 ? '0' : '') + minutes} : ${(seconds < 10 ? '0' : '') + seconds}`;
 }
 
 /*******************************COUNTDOWN*******************************/
@@ -14,18 +14,19 @@ document.querySelector("#time").textContent = `${minutes} : ${(seconds < 10 ? '0
 // When seconds reach 0, it decreases the minutes by 1 and resets seconds to 59. 
 // It continues until both minutes and seconds reach 0.
 function decreaseTime(){
-    if (seconds === 0){
-        if (minutes === 0){
+    if (seconds === 0) {
+        if (minutes === 0) {
             clearInterval(timerId);
             return;
         }
         minutes--;
         seconds = 59;
-    }else {
+    } else {
         seconds--;
         console.log(seconds);
     }
 }
+
 /*******************************START*******************************/
 const startPomodoro = document.getElementById('start');
 startPomodoro.addEventListener('click', start);
@@ -34,7 +35,7 @@ function start(){
     if (!isRunning) {
         isRunning = true;
         document.querySelector("#start").disabled = true; // Disables the Start button after it's clicked and keeps it disabled until the timer finishes
-        timerId = setInterval(( ) =>{
+        timerId = setInterval( () => {
             decreaseTime();
             displayTimer();
             if (minutes === 0 && seconds === 0) {
@@ -43,7 +44,8 @@ function start(){
             }
         }, 1000);
     }
-};
+}
+
 /*******************************PAUSE*******************************/
 const pausePomodoro = document.getElementById('pause');
 pausePomodoro.addEventListener('click', () => { 
@@ -61,4 +63,32 @@ resetPomodoro.addEventListener('click', () => {
         displayTimer(); //refreshes the display
         document.querySelector("#start").disabled = false; //re-enable the start button
     }
+});
+
+/***************************5-MINUTES BREAK***************************/
+//Note: this way our break can only be activated IF the session is/has been running
+//Later we may consider making reset work when the break is on, to reset to 05 : 00
+const shortPomodoroBreak = document.getElementById('short-break');
+shortPomodoroBreak.addEventListener('click', () => {
+    document.querySelector("#short-break").disabled = true; //disables the short break button
+    
+    if (isRunning) {
+        clearInterval(timerId); //stops the timer loop if it's running
+    }
+    //once it's cleared the following happens:
+    isRunning = true; //security check
+    minutes = 5; //update the value of the variable 
+    seconds = 0;
+    displayTimer(); //shows directly 05:00
+
+    timerId = setInterval( () => {
+            decreaseTime();
+            displayTimer();
+            if (minutes === 0 && seconds === 0) {
+                clearInterval(timerId);
+                isRunning = false; //this is optional, just security
+                document.querySelector("#short-break").disabled = false; //re-enable the break button
+                document.querySelector("#start").disabled = false; //re-enable the start button
+            }
+        }, 1000);
 });
